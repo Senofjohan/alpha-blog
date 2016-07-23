a11535ba2baeaf3c4081c86eba56f11dec7b5022
Josh Johansen <joshjohansen42@gmail.com>
2016-07-22 18:43:33 -0700

    Add admin user functionality

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -55,7 +55,7 @@ class ProblemsController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @problem.user
+      if current_user != @problem.user and !current_user.admin?
         flash[:danger] = "You can only edit or delete your own problems"
       end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :problems
+  has_many :problems, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
             uniqueness: {case_sensitive: false}, 
